45bbe0bbdd6bb170ffb21d26175bcd115c1c1e6a
giuserizz <grizzi@leggedrobotics.com>
2020-10-24 14:46:38 +0200

    kinova task space controller

---
+++ kinova_task_space_controller/include/kinova_task_space_controller/task_space_controller.h
@@ -0,0 +1,74 @@
+/*!
+ * @file     task_space_controller.h
+ * @author   Giuseppe Rizzi
+ * @date     24.10.2020
+ * @version  1.0
+ * @brief    description
+ */
+#pragma once
+
+#include <robot_control/modeling/robot_wrapper.h>
+#include <robot_control/controllers/end_effector_controllers/task_space_controller.h>
+
+#include <geometry_msgs/PoseStamped.h>
+#include <realtime_tools/realtime_publisher.h>
+
+#include <kortex_driver/non-generated/kortex_command_interface.h>
+#include <hardware_interface/robot_hw.h>
+#include <hardware_interface/joint_state_interface.h>
+#include <controller_interface/controller.h>
+
+#include <interactive_markers/interactive_marker_server.h>
+
+namespace kinova_controllers {
+
+class KinovaTaskSpaceController : public controller_interface::Controller<hardware_interface::KortexCommandInterface> {
+ public:
+  ~KinovaTaskSpaceController();
+
+  bool init(hardware_interface::KortexCommandInterface* robot_hw,
+      ros::NodeHandle& node_handle,
+      ros::NodeHandle& ctrl_handle) override;
+
+  void newTargetCallback(const geometry_msgs::PoseStamped&);
+
+  void starting(const ros::Time&) override;
+  void update(const ros::Time&, const ros::Duration& period) override;
+  void publishRos();
+
+  void control_thread();
+  void publish_thread();
+
+  void getJointPositions(Eigen::VectorXd& q) const;
+  void getJointVelocities(Eigen::VectorXd& qd) const;
+
+ protected:
+  const int dof = 7;
+  Eigen::VectorXd q_;
+  Eigen::VectorXd qd_;
+  Eigen::VectorXd cmd_;
+
+  std::shared_ptr<rc::RobotWrapper> robot_wrapper;
+  std::shared_ptr<rc::TaskSpaceController> controller;
+
+  std::vector<hardware_interface::JointStateHandle> state_handles_;
+  std::vector<hardware_interface::KortexCommandHandle> joint_handles_;
+
+  std::string joint_names_[7] = {"joint1", "joint2", "joint3", "joint4", "joint5",
+                                   "joint6", "joint7"};
+
+  std::string frame_id_ = "world";
+  std::string controlled_frame_;
+  pin::SE3 target_pose_;
+  pin::SE3 current_pose_;
+
+  ros::Subscriber target_subscriber_;
+
+  bool publish_ros_;
+  double publish_ros_rate_;
+  std::thread publish_thread_;
+  std::unique_ptr<realtime_tools::RealtimePublisher<geometry_msgs::PoseStamped>> pose_publisher_;
+
+};
+
+}
---
+++ kinova_task_space_controller/src/task_space_controller.cpp
@@ -0,0 +1,132 @@
+/*!
+ * @file     task_space_controller.cpp
+ * @author   Giuseppe Rizzi
+ * @date     24.10.2020
+ * @version  1.0
+ * @brief    description
+ */
+
+#include "kinova_task_space_controller/task_space_controller.h"
+
+#include <cmath>
+#include <ros/ros.h>
+#include <Eigen/Dense>
+
+#include <controller_interface/controller_base.h>
+#include <pluginlib/class_list_macros.h>
+#include <pinocchio/spatial/se3.hpp>
+
+namespace kinova_controllers {
+
+KinovaTaskSpaceController::~KinovaTaskSpaceController(){
+  if (publish_thread_.joinable()) publish_thread_.join();
+}
+
+bool KinovaTaskSpaceController::init(
+    hardware_interface::KortexCommandInterface* robot_hw,
+    ros::NodeHandle& node_handle,
+    ros::NodeHandle& ctrl_handle) {
+  std::string robot_description;
+  if (!node_handle.getParam("/kinova_no_gripper", robot_description)) {
+    ROS_ERROR_STREAM("Can't read robot description.");
+    return false;
+  }
+  if (!ctrl_handle.getParam("controlled_frame", controlled_frame_)) {
+    ROS_ERROR_STREAM("Set the controlled_frame parameter.");
+    return false;
+  }
+
+  if (!ctrl_handle.getParam("publish_ros", publish_ros_)) {
+    ROS_ERROR_STREAM("Set the publish_ros parameter.");
+    return false;
+  }
+
+  if (!ctrl_handle.getParam("publish_ros", publish_ros_)) {
+    ROS_ERROR_STREAM("Set the publish_ros parameter.");
+    return false;
+  }
+
+  if (!ctrl_handle.getParam("publish_ros_rate", publish_ros_rate_)) {
+    ROS_ERROR_STREAM("Set the publish_ros_rate parameter.");
+    return false;
+  }
+
+  robot_wrapper = std::make_shared<rc::RobotWrapper>();
+  robot_wrapper->initFromXml(robot_description);
+  controller = std::make_shared<rc::TaskSpaceController>(robot_wrapper, controlled_frame_);
+
+  for (auto& joint_name : joint_names_) {
+    joint_handles_.push_back(robot_hw->getHandle(joint_name));
+  }
+
+  std::string current_pose_topic = ctrl_handle.param<std::string>("current_pose_topic", "/current_pose");
+  pose_publisher_ = std::make_unique<realtime_tools::RealtimePublisher<geometry_msgs::PoseStamped>>(node_handle, current_pose_topic, 10);
+
+  std::string target_pose_topic = ctrl_handle.param<std::string>("target_pose_topic", "/target_pose");
+  target_subscriber_ = node_handle.subscribe("/target_pose", 10, &KinovaTaskSpaceController::newTargetCallback, this);
+
+  if (publish_ros_)
+    publish_thread_ = std::thread(&KinovaTaskSpaceController::publish_thread, this);
+  return true;
+}
+
+void KinovaTaskSpaceController::newTargetCallback(const geometry_msgs::PoseStamped& msg) {
+  Eigen::Vector3d translation(msg.pose.position.x, msg.pose.position.y, msg.pose.position.z);
+  Eigen::Quaterniond rotation(msg.pose.orientation.w, msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z);
+  target_pose_ = pin::SE3(rotation, translation);
+  controller->setTaskTarget(target_pose_);
+}
+
+void KinovaTaskSpaceController::starting(const ros::Time& time) {
+  for (size_t i=0; i < dof; i++) {
+    joint_handles_[i].setMode(hardware_interface::KortexControlMode::EFFORT);
+  }
+}
+
+void KinovaTaskSpaceController::update(const ros::Time& time, const ros::Duration& period) {
+  getJointPositions(q_);
+  getJointVelocities(qd_);
+  cmd_ = controller->advance(q_, qd_);
+  for (size_t i=0; i < dof; i++) {
+    joint_handles_[i].setEffortCommand(cmd_[i]);
+  }
+}
+
+void KinovaTaskSpaceController::getJointVelocities(Eigen::VectorXd& q) const {
+  for(size_t i=0; i < dof; i++){
+    q(i) = joint_handles_[i].getVelocity();
+  }
+}
+
+void KinovaTaskSpaceController::getJointPositions(Eigen::VectorXd& qd) const {
+  for(size_t i=0; i< dof; i++) {
+    qd(i) = joint_handles_[i].getPosition();
+  }
+}
+
+void KinovaTaskSpaceController::publish_thread() {
+  ros::Rate rate(publish_ros_rate_);
+  while (ros::ok()){
+    if (pose_publisher_->trylock()){
+      current_pose_ = robot_wrapper->getFramePlacement(controlled_frame_);
+      pose_publisher_->msg_.header.stamp = ros::Time::now();
+      pose_publisher_->msg_.header.frame_id = frame_id_;
+      pose_publisher_->msg_.pose.position.x = current_pose_.translation()(0);
+      pose_publisher_->msg_.pose.position.y = current_pose_.translation()(1);
+      pose_publisher_->msg_.pose.position.z = current_pose_.translation()(2);
+      Eigen::Quaterniond q(current_pose_.rotation());
+      pose_publisher_->msg_.pose.orientation.x = q.x();
+      pose_publisher_->msg_.pose.orientation.y = q.y();
+      pose_publisher_->msg_.pose.orientation.z = q.z();
+      pose_publisher_->msg_.pose.orientation.w = q.w();
+      pose_publisher_->unlockAndPublish();
+    }
+    rate.sleep();
+  }
+}
+
+}
+
+PLUGINLIB_EXPORT_CLASS(kinova_controllers::KinovaTaskSpaceController, controller_interface::ControllerBase)
+
+
